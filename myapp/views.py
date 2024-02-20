10dac457f56641f405673502c0526cf4e87f2707
Joe Wireman <wiremanj20@students.ecu.edu>
2024-02-19 22:10:16 -0500

    adds ip based location resolution

--- myapp/views.py
+++ myapp/views.py
@@ -8,21 +8,37 @@ def home(request):
   return HttpResponse(template.render())
 
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
 def weather(request):
-    # API_KEY = 'e87eb99c5ab7bd9404658726884e70ec'
+    #client_ip = get_client_ip(request)
+    client_ip = '24.48.0.1'
     API_KEY = os.environ.get('OPEN_WEATHER_API')  # Use the environment variable
     CITY_NAME = 'Greenville'
-    URL = f'http://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}'
+    URL_IP = f'http://ip-api.com/json/{client_ip}'
+    response = requests.get(URL_IP)
+    data_ip = response.json()
+
+    lat = data_ip['lat']
+    lon = data_ip['lon']
     
-    response = requests.get(URL)
-    data = response.json()
-    print(data)
-    print(os.environ.get('OPENWEATHER_API_KEY'))
-    
+    URL_Weather = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}'
+    response = requests.get(URL_Weather)
+    data_weather = response.json()
+
     weather = {
-        'description': data['weather'][0]['description'],
-        'temperature': data['main']['temp'],
+        'description': data_weather['weather'][0]['description'],
+        'temperature': data_weather['main']['temp'],
         'city': CITY_NAME
     }
+
+
     
-    return render(request, 'weather.html', {'weather_data': json.dumps(data, indent=4)})
+    return render(request, 'weather.html', {'weather_data': json.dumps(data_weather, indent=4)})
